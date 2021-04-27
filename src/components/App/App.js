7e84153463607c733a9cd713c0f6c0ddb03035c0
Chloé Danisevicius <chloe.danisevicius@gmail.com>
2021-04-27 19:58:57 +0200

    feat(app): did Axios config and first calls to the API. Set up my local state and my useEffect

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,22 +1,43 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
+import { debounce } from 'lodash-es';
 import './App.css';
 
+const PIXABAY_URL = process.env.REACT_APP_PIXABAY_URL
+
 function App() {
+  // Our local state
+  // The user's search
+  const [inputValue, setInputValue] = useState("pink");
+  // Our array of images, send by the API
+  const [images, setImages] = useState([]);
+
+  // Function who calls the API of Pixabay
+
+  useEffect(() => {
+    async function loadImages() {
+      try {
+        const response = await Axios({
+          method: 'GET',
+          url: `${PIXABAY_URL}&q=${inputValue ? inputValue : '%27%27'}`
+        });
+        if (response.status !== 200) return console.error('ERROR');
+        setImages(response.data.hits);
+        console.log("La réponse API :", response);
+
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    loadImages();
+  }, [inputValue]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input type="text" onChange={debounce((event) => {
+          setInputValue(event.target.value);
+        }, 1000)}/>
       </header>
     </div>
   );

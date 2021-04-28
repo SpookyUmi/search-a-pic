be2fb8c5a445f44267887a4e73d75484617ff0dc
Chloé Danisevicius <chloe.danisevicius@gmail.com>
2021-04-28 18:18:52 +0200

    feat(results): add style to the gallery layout

--- src/components/Results/index.js
+++ src/components/Results/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import "./styles.scss";
 
 const Results = ({ images }) => (
-  <section className="results__container">
+  <section className="images__gallery">
     {images.map(image => (
       <div
-        className="results__image__container"
+        className="images__gallery__item"
         key={image.id}
       >
         <img
-          className="results__image"
+          className="pixabay__image"
           src={image.largeImageURL}
           alt={image.tags}
         />
